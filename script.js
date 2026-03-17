3977017a8fc44166c3b35d03c40b87dfa8ace5c7
samr7672 <shivamkumar3214rew@gmail.com>
2026-03-18 01:33:31 +0530

    started the project

---
+++ script.js
@@ -0,0 +1,13 @@
+const myLibrary=[];
+
+function book(id,author, title ,  status){
+    this.id=crypto.randomUUID();
+    this.author=author;
+    this.title=title;
+    thiis.status=status;
+
+}
+function myBook(){
+    
+}
+
