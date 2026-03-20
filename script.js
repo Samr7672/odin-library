c9d3008b23657bf8d0a381cf8a984c75829b29ce
samr7672 <shivamkumar3214rew@gmail.com>
2026-03-20 17:49:44 +0530

    last stage for javascript

--- script.js
+++ script.js
@@ -1,13 +1,52 @@
-const myLibrary=[];
+{const myLibrary=[];
 
 function book(id,author, title ,  status){
     this.id=crypto.randomUUID();
     this.author=author;
     this.title=title;
-    thiis.status=status;
+    this.status=status;
 
 }
 function myBook(){
-    
+    let title=document.querySelector("#title").value;
+    let author=document.querySelector("#Author").value;
+
+    let status=document.querySelector("#status").checked;
+
+    const newBook= new book(author,title,status);
+    myLibrary.push(newBook);
+
+}
+function bookCard{
+    const card=document.querySelector(".library-display");
+    card. innerHTML=" ";
+    myLibrary.array.forEach((book) => {
+        const bookdiv=document.createElement("div")
+        bookdiv.classList.add("card");
+        
+
+        
+    });
 }
 
+const button=document.querySelector(".btn");
+
+button.addEventListener("click",()=>{
+    const form=document.querySelector(".new-form")
+    form.style.display="block";
+
+    console.log("hello")
+
+});
+const btn=document.querySelector("form")
+    btn.addEventListener("submit",(event)=>{
+        console.log("hello")
+        event.preventDefault();
+        myBook(event);
+
+    })
+
+
+
+
+}
